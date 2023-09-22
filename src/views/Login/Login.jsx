4ecb37824b4e3a7032f2fd674c07b379fc1673fd
FranSalr <franco.salrach@gmail.com>
2023-09-21 21:33:20 -0300

    Progreso

--- src/views/Login/Login.jsx
+++
@@ -1,9 +0,0 @@
-import Form from "../../components/Form/Form";
-
-const Login = ({ login }) => (
-  <div className="login_content">
-    <Form login={login} />
-  </div>
-);
-
-export default Login;
